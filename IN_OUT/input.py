56b0397cf654d71f3b83ab5a6386c192d53d0c9a
ｱｽﾓ <1xtelescope@gmail.com>
2024-08-12 23:21:52 +0900

    add: IN_OUT

---
+++ IN_OUT/input.py
@@ -0,0 +1,30 @@
+# 何も考えずに使う
+import sys
+input = sys.stdin.readline
+
+
+# 1個の整数
+n = int(input().split())
+
+# 2個の整数たち
+a, b = map(int, input().split())
+
+# 1行の整数たち
+a = list(map(int, input().split()))
+
+# 複数行の整数たち
+a = [int(input().split()) for _ in range(n)]
+
+
+# 1行の文字列たち
+s = input().split()
+
+# 複数行の文字列たち
+s = [input().split() for _ in range(n)]
+
+
+# 1個の文字列を一文字ずつに分解して格納
+c = list(input().split())
+
+# 複数行の文字列を一文字ずつに分解して格納
+c = [list(input().split()) for _ in range(n)]
